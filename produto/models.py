64310be60b560697e2d5c0642334fa101e28b299
Leonardo Breda <leonardo-breda1@hotmail.com>
2024-11-18 09:06:18 -0300

    Slug automatico dos produtos (slugify)

--- produto/models.py
+++ produto/models.py
@@ -2,20 +2,29 @@ from django.db import models
 from PIL import Image
 import os
 from django.conf import settings
+from django.utils.text import slugify
 
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
-    descricao_curta = models.TextField(max_length=255)
-    descricao_longa = models.TextField()
+    descricao_curta = models.TextField(max_length=255, verbose_name='Descrição Curta')
+    descricao_longa = models.TextField(verbose_name='Descrição Longa')
     imagem = models.ImageField(upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
-    slug = models.SlugField(unique=True)
-    preco_marketing = models.FloatField()
-    preco_marketing_promocional = models.FloatField(default=0)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    preco_marketing = models.FloatField(verbose_name='Preço')
+    preco_marketing_promocional = models.FloatField(default=0, verbose_name='Preço Promo')
     tipo = models.CharField(default='V', max_length=1, choices=(
-        ('V', 'Variação'),
+        ('V', 'Variável'),
         ('S', 'Simples'),
     ))
+
+    def get_preco_formatado(self):
+        return f'R${self.preco_marketing:.2f}'.replace('.', ',')
+    get_preco_formatado.short_description = 'Preço'
+
+    def get_preco_promocional_formatado(self):
+        return f'R${self.preco_marketing_promocional:.2f}'.replace('.', ',')
+    get_preco_promocional_formatado.short_description = 'Preço Promo'
 
     @staticmethod
     def resize_image(img, new_width=800):
@@ -33,6 +42,10 @@ class Produto(models.Model):
         new_img.save(img_full_path, optimize=True, quality=50)
 
     def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.nome)}'
+            self.slug = slug
+
         super().save(*args, **kwargs)
 
         max_image_size = 800
